0b4ed544108a8fb87d281416ace0f8905ad32be4
mckeont <mckeont@upenn.edu>
2020-05-08 18:52:51 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,203 @@
+// var myStyle = function(feature) {
+//   // return {fillColor: 'red'};
+// switch(feature.properties.name) {
+//   case 'Lou Wolf Lot, next to rec center, HIGH PRIORITY': return {color: "red", fillOpacity: 3, weight: 1};
+//   default: return {
+//     color:"MidnightBlue",
+//     fillOpacity: 1,
+//     weight: 1
+//   };
+// }
+// };
+
+
+var bufferStyle = {
+    "color": "orange",
+    "weight": 5,
+    "opacity": 6
+};
+//Highlighting Feature
+function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 5,
+        color: 'MidnightBlue',
+        dashArray: '',
+        fillOpacity: 0.3
+    });
+  }
+function resetHighlight(e) {
+      geojson.resetStyle(e.target);
+  }
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
+}
+
+  //new icon parameters
+
+// $(document).ready(function() {
+
+// var pennLogo = L.icon({
+//       iconUrl: 'images/pennLogo.png', //source, online search
+//       iconSize: [64, 64],
+//       iconAnchor: [0, 0],
+//       popupAnchor: [-1, -5],
+// });
+// console.log(pennLogo);
+// //
+// //
+
+
+//  var geojson = L.geoJson(zip, {
+//   style: myStyle,
+//   onEachFeature: onEachFeature
+// }).bindPopup(function (layer) {
+//     return layer.feature.properties.CODE;
+// }).addTo(map);
+
+var geojson = L.geoJson(wells, {
+ // style: myStyle,
+ // onEachFeature: onEachFeature
+}).bindPopup(function (layer) {
+   return "Operator: " + layer.feature.properties.OPERATOR + "<dd>"
+   + "OGO: " + layer.feature.properties.OGO + "<dd>" +
+   "Farm: " + layer.feature.properties.FARM ;
+}).addTo(map);
+
+ var geojson = L.geoJson(clipstream, {
+ onEachFeature: onEachFeature,
+ // style: streamStyle
+}).bindPopup(function (layer) {
+   return "Stream Name: " + layer.feature.properties.Name
+}).addTo(map);
+
+L.geoJson(buffer, {
+ style: bufferStyle
+ // onEachFeature: onEachFeature
+}).addTo(map).bringToBack();
+
+
+//
+// L.geoJson(lancaster,{
+// onEachFeature: onEachFeature
+// }).addTo(map);
+
+// var cleanUp = function(feature, layer) {
+//    onEachFeature(if(typefeature.properties.Lead_B == undefined || feature.properties.Lead_B == null || feature.properties.Lead_B == 0 ) return ;)
+//
+// }
+
+// console.log(cleanUp);
+
+// var sampleSite = {
+//     radius: 5,
+//     fillColor: "#ff7800",
+//     color: "#000",
+//     weight: 1,
+//     opacity: 1,
+//     fillOpacity: 0.7
+// };
+//
+//
+//
+// L.geoJson(leadSample, {
+//   pointToLayer: function(feature, latlng) {
+//               return L.circleMarker(latlng, sampleSite);
+//            }
+//
+// }).bindPopup(function (layer) {
+//
+//   var result = ( "Sample Date:" + " " + layer.feature.properties.Date );
+//   result = result + ("<dd>" + "<em>" + "Parts Per Million" + "</em>" + "</dd>");
+//   result = result + ("<dd>" + "Sample A:" + " " + layer.feature.properties.Lead_A + "</dd>");
+//
+// if(layer.feature.properties.Lead_B != undefined && layer.feature.properties.Lead_B != null && layer.feature.properties.Lead_B != 0)
+//  result = result + ("<dd>" + "Sample B:" + " " + layer.feature.properties.Lead_B + "</dd>");
+//
+//  if(layer.feature.properties.Lead_C != undefined && layer.feature.properties.Lead_C != null && layer.feature.properties.Lead_C != 0)
+//   result = result + ("<dd>" + "Sample C:" + " " + layer.feature.properties.Lead_C + "</dd>");
+//
+//   if(layer.feature.properties.Lead_D != undefined && layer.feature.properties.Lead_D != null && layer.feature.properties.Lead_D != 0)
+//    result = result + ("<dd>" + "Sample D:" + " " + layer.feature.properties.Lead_D + "</dd>");
+//
+//    if(layer.feature.properties.Lead_E != undefined && layer.feature.properties.Lead_E != null && layer.feature.properties.Lead_E != 0)
+//     result = result + ("<dd>" + "Sample E:" + " " + layer.feature.properties.Lead_E + "</dd>");
+//
+// return result;
+//  }).addTo(map);
+//
+//
+//  console.log(leadSample);
+//
+//
+//
+//  // Discalimer clarifiying that there is a level
+//
+//   //testing out console.log
+//
+//   console.log("What is the next clue?:");
+//
+//   if (window.console && window.console.log) {
+//   // console is available
+// }
+//
+//
+// //buttons_
+// $(document).ready(function() {
+//  //Button Zoom
+//   $("#lanc").click(function(){
+//      map.setView([40.044437, -76.306229]);
+//      map.setZoom(10);
+//   });
+//
+//   $("#philly").click(function(){
+//      map.setView([39.987614, -75.163913]);
+//      map.setZoom(10);
+//   });
+//
+//   $('#fennsTreasure').click(function(){
+//     window.open(url, "http://fennclues.com/hints-and-clues-fenn-treasure.html");
+//   });
+//
+//
+// });
+//
+//
+// var popTest = "<a href ='images/Health_Quilt_forBlog.jpg'> Next Clue </a>"
+//
+//   // Creating circles
+//   var circle = L.circle([36.715736, -107.161087], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500,
+//
+// }).bindPopup("<h4 style= 'color:orange'> You found an Easter Egg! </h4>" + "<br>" +"<dd>"+
+// popTest + " " + "<br>" + "<h3><b>poems</b></h3>" ).addTo(map);
+//
+//
+// var fenn = [];
+// for ( var i=1.20, l=2.5; i<l; i+=0.01 ){
+//
+//     fenn.push(i.toFixed(2));
+//
+// }
+//
+// heights = fenn.join('\n');
+//
+// var poems =  "Begin it where warm waters halt" + " "
+// + "And take it in the canyon down," + " "
+// + "Not far, but too far to walk." + " "
+// + "Put in below the home of Brown."
+//
+
+
+
+// });
+
+// .bindPopup(function (layer) {
+//     return (layer.feature.properties.title);
+//   })
---
+++ js/wells.js
@@ -0,0 +1,557 @@
+var wells = {
+   "type": "FeatureCollection",
+   "features": [
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.871074,41.713119 ]
+    },
+    "properties": {
+    "OGO":"OGO-10897",
+    "OPERATOR":"CABOT OIL & GAS CORP",
+    "FARM":"TEEL 5"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.874872,41.703372 ]
+    },
+    "properties": {
+    "OGO":"OGO-10897",
+    "OPERATOR":"CABOT OIL & GAS CORP",
+    "FARM":"TEEL 10V"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.674281,41.731164 ]
+    },
+    "properties": {
+    "OGO":"OGO-10897",
+    "OPERATOR":"CABOT OIL & GAS CORP",
+    "FARM":"K ZUPP 1"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.93535,41.817733 ]
+    },
+    "properties": {
+    "OGO":"OGO-67055",
+    "OPERATOR":"CARRIZO (MARCELLUS) LLC",
+    "FARM":"SHASKAS SOUTH 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.955286,41.813431 ]
+    },
+    "properties": {
+    "OGO":"OGO-67055",
+    "OPERATOR":"CARRIZO (MARCELLUS) LLC",
+    "FARM":"HENNINGER 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.415442,41.794228 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"WYGRALA 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.467044,41.776586 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"ELEVATION 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.439472,41.626117 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"HENRY 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.461031,41.668614 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"JOHNSON 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.364458,41.813225 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"ALLEN 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.320056,41.873744 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"HAYWARD 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.0126,41.618111 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"SLUMBER VALLEY 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.588044,41.697131 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"BARCLAY 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.598531,41.696836 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"MEL BRA 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.667281,41.680428 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"NEAL BRA 6H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.055008,41.632147 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"STINE WYO 7H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.076389,41.579408 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"VISNESKI WYO 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -75.799106,41.682786 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"PHELPS UNIT 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.808778,41.658389 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"SECHRIST UNIT 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.675161,41.679172 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"YODER UNIT 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.644114,41.686794 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"POSTELL UNIT A 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.512294,41.708164 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"ARNOLD UNIT A 3H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.9639,40.660144 ]
+    },
+    "properties": {
+    "OGO":"OGO-61555",
+    "OPERATOR":"EQT PRODUCTION CO",
+    "FARM":"ROYAL OIL & GAS 590333"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.600611,41.237861 ]
+    },
+    "properties": {
+    "OGO":"OGO-39315",
+    "OPERATOR":"EXCO RESOURCES PA LLC",
+    "FARM":"WARNER UNIT 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.960689,40.604628 ]
+    },
+    "properties": {
+    "OGO":"OGO-33314",
+    "OPERATOR":"BLX INC",
+    "FARM":"R SLEPPY 1-86.6"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.65575,41.828917 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"HORST 6H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.309322,41.877794 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"REXFORD 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.245683,41.600153 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"MOBEAR 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.537628,41.788847 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"LOMISON INC BRA 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.055061,41.632158 ]
+    },
+    "properties": {
+    "OGO":"OGO-65420",
+    "OPERATOR":"CHESAPEAKE APPALACHIA LLC",
+    "FARM":"STINE WYO 8H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.717925,41.590411 ]
+    },
+    "properties": {
+    "OGO":"OGO-66495",
+    "OPERATOR":"CHIEF OIL & GAS LLC",
+    "FARM":"IDC-INNES UNIT 4H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.91185,41.310381 ]
+    },
+    "properties": {
+    "OGO":"OGO-68657",
+    "OPERATOR":"INFLECTION ENERGY (PA) LLC",
+    "FARM":"NATURE BOY COSTELLO 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.372937,41.720399 ]
+    },
+    "properties": {
+    "OGO":"OGO-63025",
+    "OPERATOR":"PA GEN ENERGY CO LLC",
+    "FARM":"POTATO CREEK 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -80.367778,40.250996 ]
+    },
+    "properties": {
+    "OGO":"OGO-60915",
+    "OPERATOR":"RANGE RESOURCES APPALACHIA LLC",
+    "FARM":"LOWRY WILLIAM UNIT 2H-A"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -80.378639,40.298717 ]
+    },
+    "properties": {
+    "OGO":"OGO-60915",
+    "OPERATOR":"RANGE RESOURCES APPALACHIA LLC",
+    "FARM":"LEHMAN UNIT 1"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.279994,41.964997 ]
+    },
+    "properties": {
+    "OGO":"OGO-63815",
+    "OPERATOR":"REPSOL OIL & GAS USA LLC",
+    "FARM":"WHEATON 05 223 01 W 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.834231,41.808906 ]
+    },
+    "properties": {
+    "OGO":"OGO-63815",
+    "OPERATOR":"REPSOL OIL & GAS USA LLC",
+    "FARM":"FEUSNER 03 044 05 J 5H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.912031,41.285928 ]
+    },
+    "properties": {
+    "OGO":"OGO-68657",
+    "OPERATOR":"INFLECTION ENERGY (PA) LLC",
+    "FARM":"ULTIMATE WARRIOR 3H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.565858,40.353253 ]
+    },
+    "properties": {
+    "OGO":"OGO-68579",
+    "OPERATOR":"LPR ENERGY LLC",
+    "FARM":"RITCHEY UNIT 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.142968,41.780098 ]
+    },
+    "properties": {
+    "OGO":"OGO-68607",
+    "OPERATOR":"ROULETTE OIL & GAS LLC",
+    "FARM":"COLUSSY YX201"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.056101,41.579631 ]
+    },
+    "properties": {
+    "OGO":"OGO-68673",
+    "OPERATOR":"WARREN E & P INC",
+    "FARM":"PROCTOR & GAMBLE MEHOOPANY PLT P&G3 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.629721,41.221471 ]
+    },
+    "properties": {
+    "OGO":"OGO-38958",
+    "OPERATOR":"XTO ENERGY INC",
+    "FARM":"TEMPLE 8496H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.632069,41.199694 ]
+    },
+    "properties": {
+    "OGO":"OGO-38958",
+    "OPERATOR":"XTO ENERGY INC",
+    "FARM":"TOME 8522H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -76.543742,41.2423 ]
+    },
+    "properties": {
+    "OGO":"OGO-38958",
+    "OPERATOR":"XTO ENERGY INC",
+    "FARM":"RENN UNIT A 1H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.067139,41.037944 ]
+    },
+    "properties": {
+    "OGO":"OGO-39155",
+    "OPERATOR":"XPR RESOURCES LLC",
+    "FARM":"ALDER RUN LAND LP 2H"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -78.077139,41.043611 ]
+    },
+    "properties": {
+    "OGO":"OGO-39155",
+    "OPERATOR":"XPR RESOURCES LLC",
+    "FARM":"ALDER RUN LAND LP 3H"
+    }
+  }
+]
+};
