c16abb71655b45e117ffcd0bfa3840ecf2273385
mckeont <mckeont@upenn.edu>
2020-05-09 09:28:13 -0400

    update

--- js/setup.js
+++ js/setup.js
@@ -14,9 +14,9 @@ var Satellite  = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
-var baseMaps = {
-    "Satellite": Satellite,
-    "Terrain": Terrain
-};
+// var baseMaps = {
+//     "Satellite": Satellite,
+//     "Terrain": Terrain
+// };
 
 // L.control.layers(baseMaps).addTo(map);
